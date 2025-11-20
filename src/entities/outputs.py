76b1e8105642072657b9df43ad66eb5f96c9b268
shivws <kotent12345@gmail.com>
2025-11-21 01:21:23 +0300

    laba 5 | rename files

--- src/entities/outputs.py
+++ src/entities/outputs.py
@@ -23,26 +23,26 @@ class Outputs:
 
 
     def json_ser(self) -> None:
-        with open('out/workers.json', 'w') as f: f.write('')
+        with open('out/data.json', 'w') as f: f.write('')
         data = []
         for worker in self.workers.workers:
             rights = [{'uuid': right.uuid, 'name': right.name, 'description': right.description} for right in worker.position.rights]
             position = {'uuid': worker.position.uuid, 'name': worker.position.name, 'description': worker.position.description, 'rights': rights}
             data.append({'uuid': worker.uuid, 'sex': worker.sex, 'name': worker.name, 'birthday': worker.birthday, 'position': position})
-        with open('out/workers.json', 'a', encoding='UTF-8') as f:
+        with open('out/data.json', 'a', encoding='UTF-8') as f:
             json.dump(data, f, indent='    ', ensure_ascii=False)
         return None
 
 
     def csv_ser(self) -> None:
-        with open('out/workers.csv', 'w') as f: f.write('')
+        with open('out/data.csv', 'w') as f: f.write('')
         data = []
         for worker in self.workers.workers:
             rights = [[str(right.uuid), right.name, right.description] for right in worker.position.rights]
             position = [worker.position.uuid, worker.position.name, worker.position.description]
             for right in rights: position.extend(right)
             data.append([worker.uuid, worker.sex, worker.name, worker.birthday, *position])
-        with open('out/workers.csv', 'a', encoding='UTF-8') as f:
+        with open('out/data.csv', 'a', encoding='UTF-8') as f:
             writer = csv.writer(f)
             writer.writerow(['uuid','sex','name','birthday','position.uuid','position.name','position.description','right.uuid','right.name','right.description','...'])
             writer.writerows(data)
@@ -50,7 +50,7 @@ class Outputs:
 
 
     def xml_ser(self) -> None:
-        with open('out/workers.xml', 'w') as f: f.write('')
+        with open('out/data.xml', 'w') as f: f.write('')
         data = []
         for worker in self.workers.workers:
             rights = {}
@@ -64,7 +64,7 @@ class Outputs:
             self.xml_recur_pars(builder, worker)
         tree = ElementTree(builder.close())
         indent(tree, space='    ')
-        tree.write('out/workers.xml', encoding='UTF-8', xml_declaration=True)
+        tree.write('out/data.xml', encoding='UTF-8', xml_declaration=True)
         return None
 
 
@@ -80,6 +80,6 @@ class Outputs:
 
 
     def yaml_ser(self) -> None:
-        with open('out/workers.yaml', 'w') as f: f.write('')
-        with open('out/workers.yaml', 'a', encoding='UTF-8') as f:
+        with open('out/data.yaml', 'w') as f: f.write('')
+        with open('out/data.yaml', 'a', encoding='UTF-8') as f:
             yaml.dump_all(self.workers.workers, f, indent=4, allow_unicode=True)
